f09640a56bafdbb28586861ca7379b07ec96d7e3
sarthakbhattarai <sarthakbhattarai@Sarthaks-MacBook-Pro.local>
2026-07-18 16:00:24 +0545

    updated the json acceptable storage

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 const server = http.createServer(app);
 
