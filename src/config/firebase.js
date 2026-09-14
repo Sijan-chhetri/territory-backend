f676c5d1f5b1584b51d7f5e6bfa03f7595a8038d
Sijan-chhetri <sijanchhetri102@gmail.com>
2026-09-14 11:42:40 +0545

    fcm

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebase_service.json" assert { type: "json" };
+import serviceAccount from "./firebase_service.json" with { type: "json" };
 
 if (!admin.apps.length) {
   admin.initializeApp({
