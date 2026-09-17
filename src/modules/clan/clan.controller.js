a109e96c06e88356a3ce7db8999d183c5a28d43f
Sijan-chhetri <sijanchhetri102@gmail.com>
2026-09-17 13:24:47 +0545

    caln notify

--- src/modules/clan/clan.controller.js
+++ src/modules/clan/clan.controller.js
@@ -2,7 +2,7 @@
 import { Prisma } from "@prisma/client";
 import prisma from "../../config/prisma.js";
 import { sendClanEventInvitations } from "../clanEvent/clanEventEmail.service.js";
-import { sendFCMToUser } from "../../services/fcm.service.js";
+import { sendFCMToUser } from "../../config/fcm.service.js";
 
 
 
